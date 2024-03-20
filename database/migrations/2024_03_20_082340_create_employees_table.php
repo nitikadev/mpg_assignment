111b9fe8d4f09d43a4659c2c9ed6c1c1633a793e
nitikaIMG <nitika.img@gmail.com>
2024-03-20 16:19:59 +0530

    first commit

--- database/migrations/2024_03_20_082340_create_employees_table.php
+++ database/migrations/2024_03_20_082340_create_employees_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email');
             $table->string('password');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
